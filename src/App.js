d228ec8203c64f8422b136d13a9440c2164aafe5
Kaleb <kaleb@gullele.com>
2019-11-27 11:35:49 -0500

    Simple react redirect for illustration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//stateless component for navigation
+const User = (params) => {
+return (<h3>I am another component {params.name}</h3>)
+}
+
+class App extends Component {
+  name = "john";
+
+  state = {
+    logged: false
+  }
+
+  loginHandler = () => {
+    this.setState({
+      logged: false
+    })
+    console.log('Clicked the button');
+    console.log(this.state.logged);
+  }
+
+  handleTimer() {
+    
+    setTimeout( () => {
+      console.log('redirecting..');
+      //return <Redirect to="/hello" />
+      this.setState({logged: true});
+    }, 2000 )
+  }
+
+  render() {
+    return (
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/hello">Hello</Link></li>
+          <li><Link to="/user/logger">User</Link></li>
+        </ul>
+        <button onClick={this.loginHandler}>Login</button>
+        <div>
+        {/* this is the default home url */}
+        <Route path="/" exact render = {
+          () => {
+            return (<h1>Welcome to React</h1>)
+          }
+        }/>
+        {/* this is the default hello */}
+        <Route path="/hello" exact render = {
+          () => {
+            return (<h1>Salute fellow Reactor {this.name}!!</h1>)
+          }
+        }/>
+        {/* <Route path="/user/:name" exact component={User} /> */}
+
+        {/* this handles conditional redirect. if logged is true, then it will 
+        render the component, otherwise it will redirect it to home */}
+        <Route path="/user/:name" exact render = {
+          ({match}) => {
+            return this.state.logged ? (<User name={match.params.name}/>) : (
+              <>
+              <div>SHowing this message for 2 seconds </div>
+
+                {/* handlerTime will set logged from false to true. */}
+                {this.handleTimer()}
+                {this.state.logged ? <Redirect to="/hello" /> : <h1>maybe</h1>}
+              </>
+            )
+          }
+        } />
+        </div>
+      </Router>
+      
+    );
+  }
 }
 
 export default App;
